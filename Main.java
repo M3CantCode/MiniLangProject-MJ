3329420db3ba7798923d279d1691319701d4c97a
CardKwando <13jthanm@gmail.com>
2026-09-16 12:01:21 -0400

    Updating text file "MiniLang_Program.txt" Updating main to read and print from "MiniLang_Program.txt"

--- Main.java
+++ Main.java
@@ -10,6 +10,19 @@ public class Main {
         //
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new FileReader("MiniLang_Program.txt"));
+        System.out.println(br.toString());
 
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        br.close();
+
+        String[] strings = sb.toString().split(" ");
+
+
+        for (int j = 0; j < strings.length; j++) {
+            System.out.printf("%s ", strings[j]);
+        }
     }
 }
