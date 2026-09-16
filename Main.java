14c6f23d010827c036e6d3ac5ddfb5ad57aa2a71
CardKwando <13jthanm@gmail.com>
2026-09-16 11:36:59 -0400

    Create main java file and text files to read from

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        //
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new FileReader("MiniLang_Program.txt"));
+
+    }
+}
